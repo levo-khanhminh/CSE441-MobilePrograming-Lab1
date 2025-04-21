61747e1966c368eaefb08a47887c39c66daf5979
Le Vo Khanh Minh <148637435+khanhminh-se@users.noreply.github.com>
2025-04-21 14:50:13 +0700

    Add some core components from React Native to project to test

--- Q3Expo1/App.tsx
+++ Q3Expo1/App.tsx
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState<number>(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <Button title="Increase" onPress={() => setCount(count + 1)} />
+      <Text>{count}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "400",
+    color: "red",
   },
 });
